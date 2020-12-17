8d83e6e0b5b9669c22f5867f94c2235a9657a9cb
Will Nelson <wn95880@gmail.com>
2020-12-17 02:21:01 -0500

    Psuedocode

---
+++ Main/script.js
@@ -0,0 +1,13 @@
+//Call necessary classes/IDs and store them in variables for later use
+
+//Make Start Quiz button hide the main menu elements and show the questions
+
+//Add timer functionality, timer start linked to start quiz button
+
+//Make questions detect right and wrong answers, wrong answers subtract time from the running timer, next question is shown after answer is submitted and user is notified of whether they were or were not correct
+
+//Repeat for like 4 questions
+
+//Quiz ends after all questions are finished or timer becomes 0
+
+//Score is given and will prompt for initials to save the user's scores locally
